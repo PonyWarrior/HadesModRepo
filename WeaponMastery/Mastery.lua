3d6ea4b55eac13e078256d626cbebd3a45e1f43b
Unknown <lob.revolu@gmail.com>
2020-10-01 15:20:33 +0200

    new mod project: mastery

---
+++ WeaponMastery/Mastery.lua
@@ -0,0 +1,57 @@
+
+MasteryList = {
+  SwordWeapon =
+  {
+    Name = "SwordWeapon",
+    Icon = "Codex_Portrait_Sword",
+    Aspects =
+    {
+      "SwordBaseUpgradeTrait", "SwordCriticalParryTrait", "DislodgeAmmoTrait", "SwordConsecrationTrait",
+    },
+  },
+  SpearWeapon =
+  {
+    Name = "SpearWeapon",
+    Icon = "Codex_Portrait_Spear",
+    Aspects =
+    {
+      "SpearBaseUpgradeTrait", "SpearTeleportTrait", "SpearWeaveTrait", "SpearSpinTravel",
+    },
+  },
+  ShieldWeapon =
+  {
+    Name = "ShieldWeapon",
+    Icon = "Codex_Portrait_Shield",
+    Aspects =
+    {
+      "ShieldBaseUpgradeTrait", "ShieldRushBonusProjectileTrait", "ShieldTwoShieldTrait", "ShieldLoadAmmoTrait",
+    },
+  },
+  BowWeapon =
+  {
+    Name = "BowWeapon",
+    Icon = "Codex_Portrait_Bow",
+    Aspects =
+    {
+      "BowBaseUpgradeTrait", "BowMarkHomingTrait", "BowLoadAmmoTrait", "BowBondTrait",
+    },
+  },
+  FistWeapon =
+  {
+    Name = "FistWeapon",
+    Icon = "Codex_Portrait_Fist",
+    Aspects =
+    {
+      "FistBaseUpgradeTrait", "FistVacuumTrait", "FistWeaveTrait", "FistDetonateTrait",
+    },
+  },
+  GunWeapon =
+  {
+    Name = "GunWeapon",
+    Icon = "Codex_Portrait_Gun",
+    Aspects =
+    {
+      "GunBaseUpgradeTrait", "GunGrenadeSelfEmpowerTrait", "GunManualReloadTrait", "GunLoadedGrenadeTrait",
+    },
+  },
+}
