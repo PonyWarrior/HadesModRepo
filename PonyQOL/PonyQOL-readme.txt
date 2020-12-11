77a4fcbc7ce7310c0d86a0fe0e0cd811b0bdc67f
Unknown <lob.revolu@gmail.com>
2020-12-11 11:33:53 +0100

    added bulk buy toggle to wretched broker + new sjson entry

--- PonyQOL/PonyQOL-readme.txt
+++ PonyQOL/PonyQOL-readme.txt
@@ -19,7 +19,7 @@ added pom icon to level-able boons for all menus
 added permanent chamber counter
 can no longer roll Demeter's Nourished Soul boon with Lasting Consequences 4 pact option
 
-2.0.1
+2.1.0
 
 fixed issues:
 closing the boon tray hides the chamber counter
@@ -29,3 +29,4 @@ make Lucifer beam destroy projectiles
 added weapon boon lists only listing their own hammer upgrades
 added charon boon list
 added inactive mirror upgrade to a boon's possible requirement in boon list
+added bulk buy toggle to wretched broker
--- PonyQOL/PonyQOL.lua
+++ PonyQOL/PonyQOL.lua
@@ -1274,3 +1274,227 @@ function CreateBoonLootButtons( lootData, reroll)
 		end
 	end
 end
+
+--Wretched Broker bulk buy
+function OpenMarketScreen()
+	local screen = { Components = {} }
+	screen.Name = "Market"
+	screen.NumSales = 0
+	screen.NumItemsOffered = 0
+	--Mod start
+	screen.SwapType = "Increase"
+	--Mod end
+
+	if IsScreenOpen( screen.Name ) then
+		return
+	end
+	OnScreenOpened({ Flag = screen.Name, PersistCombatUI = true })
+	FreezePlayerUnit()
+	EnableShopGamepadCursor()
+
+	PlaySound({ Name = "/SFX/Menu Sounds/DialoguePanelIn" })
+
+	local components = screen.Components
+
+	components.ShopBackgroundDim = CreateScreenComponent({ Name = "rectangle01", Group = "Combat_Menu" })
+	components.ShopBackground = CreateScreenComponent({ Name = "ShopBackground", Group = "Combat_Menu" })
+	components.CloseButton = CreateScreenComponent({ Name = "ButtonClose", Group = "Combat_Menu", Scale = 0.7 })
+	Attach({ Id = components.CloseButton.Id, DestinationId = components.ShopBackground.Id, OffsetX = 0, OffsetY = 440 })
+	components.CloseButton.OnPressedFunctionName = "CloseMarketScreen"
+	components.CloseButton.ControlHotkey = "Cancel"
+	--Mod start
+	components.TestButton = CreateScreenComponent({ Name = "BlankObstacle", Group = "Combat_Menu" })
+	Attach({ Id = components.TestButton.Id, DestinationId = components.ShopBackground.Id })
+	components.TestButton.OnPressedFunctionName = "SwapMarketPrices"
+	components.TestButton.ControlHotkey = "Confirm"
+	--Mod end
+	SetScale({ Id = components.ShopBackgroundDim.Id, Fraction = 4 })
+	SetColor({ Id = components.ShopBackgroundDim.Id, Color = {0.090, 0.055, 0.157, 0.7} })
+
+	-- Title
+	CreateTextBox({ Id = components.ShopBackground.Id, Text = "MarketScreen_Title", FontSize = 32, OffsetX = 0, OffsetY = -445, Color = Color.White, Font = "SpectralSCLightTitling", ShadowBlur = 0, ShadowColor = {0,0,0,1}, ShadowOffset={0, 3}, Justification = "Center" })
+	CreateTextBox({ Id = components.ShopBackground.Id, Text = "MarketScreen_Hint", FontSize = 14, OffsetX = 0, OffsetY = 380, Width = 840, Color = Color.Gray, Font = "AlegreyaSansSCBold", ShadowBlur = 0, ShadowColor = {0,0,0,1}, ShadowOffset={0, 2}, Justification = "Center" })
+
+	-- Flavor Text
+	local flavorTextOptions = { "MarketScreen_FlavorText01", "MarketScreen_FlavorText02", "MarketScreen_FlavorText03", }
+	local flavorText = GetRandomValue( flavorTextOptions )
+	CreateTextBox({ Id = components.ShopBackground.Id, Text = flavorText,
+			FontSize = 16,
+			OffsetY = -385, Width = 840,
+			Color = {0.698, 0.702, 0.514, 1.0},
+			Font = "AlegreyaSansSCExtraBold",
+			ShadowBlur = 0, ShadowColor = {0,0,0,0}, ShadowOffset={0, 3},
+			Justification = "Center" })
+	--Mod start
+	CreateTextBox({ Id = components.ShopBackground.Id, Text = "Press Enter(PC), Square(PS) or X(Xbox) to increase and decrease amounts",
+			FontSize = 16,
+			OffsetY = -320, Width = 840,
+			Color = {0.698, 0.702, 0.514, 1.0},
+			Font = "AlegreyaSansSCExtraBold",
+			ShadowBlur = 0, ShadowColor = {0,0,0,0}, ShadowOffset={0, 3},
+			Justification = "Center" })
+	--Mod end
+	-- image
+	--Mod start
+	CreateMarketButtons(screen)
+	--Mod end
+
+	if screen.NumItemsOffered == 0 then
+		thread( PlayVoiceLines, GlobalVoiceLines.MarketSoldOutVoiceLines, true )
+	else
+		thread( PlayVoiceLines, GlobalVoiceLines.OpenedMarketVoiceLines, true )
+	end
+
+	HandleScreenInput( screen )
+	return screen
+end
+
+function CreateMarketButtons(screen)
+	local components = screen.Components
+	local tooltipData = {}
+	local yScale = math.min( 3 / CurrentRun.MarketOptions , 1 )
+
+	local itemLocationStartY = ShopUI.ShopItemStartY - ( ShopUI.ShopItemSpacerY * (1 - yScale) * 0.5)
+	local itemLocationYSpacer = ShopUI.ShopItemSpacerY * yScale
+	local itemLocationMaxY = itemLocationStartY + 4 * itemLocationYSpacer
+
+	local itemLocationStartX = ShopUI.ShopItemStartX
+	local itemLocationXSpacer = ShopUI.ShopItemSpacerX
+	local itemLocationMaxX = itemLocationStartX + 1 * itemLocationXSpacer
+
+	local itemLocationTextBoxOffset = 380
+
+	local itemLocationX = itemLocationStartX
+	local itemLocationY = itemLocationStartY
+
+	local textSymbolScale = 0.8
+
+	local firstUseable = false
+	for itemIndex, item in ipairs( CurrentRun.MarketItems ) do
+
+		if not item.SoldOut then
+
+			screen.NumItemsOffered = screen.NumItemsOffered + 1
+
+			local purchaseButtonKey = "PurchaseButton"..itemIndex
+			components[purchaseButtonKey] = CreateScreenComponent({ Name = "MarketSlot", Group = "Combat_Menu", Scale = 1, X = itemLocationX, Y = itemLocationY })
+			SetInteractProperty({ DestinationId = components[purchaseButtonKey].Id, Property = "TooltipOffsetX", Value = 665 })
+
+			local iconKey = "Icon"..itemIndex
+			components[iconKey] = CreateScreenComponent({ Name = "BlankObstacle", X = itemLocationX - 360, Y = itemLocationY, Group = "Combat_Menu" })
+
+			local itemBackingKey = "Backing"..itemIndex
+			components[itemBackingKey] = CreateScreenComponent({ Name = "BlankObstacle", Group = "Combat_Menu", X = itemLocationX + itemLocationTextBoxOffset, Y = itemLocationY })
+
+			local purchaseButtonTitleKey = "PurchaseButtonTitle"..itemIndex
+			components[purchaseButtonTitleKey] = CreateScreenComponent({ Name = "BlankObstacle", Group = "Combat_Menu", Scale = 1, X = itemLocationX, Y = itemLocationY })
+
+			local costColor = {0.878, 0.737, 0.259, 1.0}
+			if not HasResource( item.CostName, item.CostAmount ) then
+				costColor = Color.TradeUnaffordable
+			end
+
+			components[purchaseButtonKey].OnPressedFunctionName = "HandleMarketPurchase"
+			if not firstUseable then
+				TeleportCursor({ OffsetX = itemLocationX, OffsetY = itemLocationY })
+				firstUseable = true
+			end
+
+			-- left side text
+			local buyResourceData = ResourceData[item.BuyName]
+			if buyResourceData then
+				components[purchaseButtonTitleKey .. "Icon"] = CreateScreenComponent({ Name = "BlankObstacle", Group = "Combat_Menu", Scale = 1 })
+				SetAnimation({ Name = buyResourceData.Icon, DestinationId = components[purchaseButtonTitleKey .. "Icon"].Id, Scale = 1 })
+				Attach({ Id = components[purchaseButtonTitleKey .. "Icon"].Id, DestinationId = components[purchaseButtonTitleKey].Id, OffsetX = -400, OffsetY = 0 })
+				components[purchaseButtonTitleKey .. "SellText"] = CreateScreenComponent({ Name = "BlankObstacle", Group = "Combat_Menu", Scale = 1 })
+				Attach({ Id = components[purchaseButtonTitleKey .. "SellText"].Id, DestinationId = components[purchaseButtonTitleKey].Id, OffsetX = 0, OffsetY = 0 })
+
+				local titleText = "MarketScreen_Entry_Title"
+				if item.BuyAmount == 1 then
+					titleText = "MarketScreen_Entry_Title_Singular"
+				end
+				CreateTextBox({ Id = components[purchaseButtonKey].Id, Text = titleText,
+					FontSize = 48 * yScale ,
+					OffsetX = -350, OffsetY = -35,
+					Width = 720,
+					Color = {0.878, 0.737, 0.259, 1.0},
+					Font = "AlegreyaSansSCMedium",
+					ShadowBlur = 0, ShadowColor = {0,0,0,1}, ShadowOffset={0, 2},
+					Justification = "Left",
+					VerticalJustification = "Top",
+					LuaKey = "TempTextData",
+					LuaValue = item,
+					LineSpacingBottom = 20,
+					TextSymbolScale = textSymbolScale,
+				})
+				CreateTextBox({ Id = components[purchaseButtonTitleKey.."SellText"].Id, Text = "MarketScreen_Cost",
+					FontSize = 48 * yScale ,
+					OffsetX = 420, OffsetY = -24,
+					Width = 720,
+					Color = costColor,
+					Font = "AlegreyaSansSCMedium",
+					ShadowBlur = 0, ShadowColor = {0,0,0,1}, ShadowOffset={0, 2},
+					Justification = "Right",
+					LuaKey = "TempTextData",
+					LuaValue = item,
+					LineSpacingBottom = 20,
+					TextSymbolScale = textSymbolScale,
+				})
+				ModifyTextBox({ Ids = components[purchaseButtonTitleKey.."SellText"].Id, BlockTooltip = true })
+
+				CreateTextBoxWithFormat({ Id = components[purchaseButtonKey].Id, Text = buyResourceData.IconString or item.BuyName,
+					FontSize = 16 * yScale,
+					OffsetX = -350, OffsetY = 0,
+					Width = 650,
+					Color = Color.White,
+					Justification = "Left",
+					VerticalJustification = "Top",
+					LuaKey = "TempTextData",
+					LuaValue = item,
+					TextSymbolScale = textSymbolScale,
+					Format = "MarketScreenDescriptionFormat",
+					VariableAutoFormat = "BoldFormatGraft",
+					UseDescription = true
+				})
+				if not item.Priority then
+					CreateTextBox({ Id = components[purchaseButtonKey].Id, Text = "Market_LimitedTimeOffer", OffsetX = 300, OffsetY = 0, FontSize = 28, Color = costColor, Font = "AlegreyaSansSCRegular", Justification = "Left", TextSymbolScale = textSymbolScale })
+				end
+			end
+
+			components[purchaseButtonKey].Data = item
+			components[purchaseButtonKey].Index = itemIndex
+			components[purchaseButtonKey].TitleId = components[purchaseButtonTitleKey].Id
+		end
+
+		itemLocationX = itemLocationX + itemLocationXSpacer
+		if itemLocationX >= itemLocationMaxX then
+			itemLocationX = itemLocationStartX
+			itemLocationY = itemLocationY + itemLocationYSpacer
+		end
+	end
+end
+
+local marketSwapType = "Increase"
+function SwapMarketPrices(screen, button)
+	if marketSwapType == "Increase" then
+		for itemIndex, item in ipairs( CurrentRun.MarketItems ) do
+			if item.Priority then
+				item.CostAmount = item.CostAmount * 10
+				item.BuyAmount = item.BuyAmount * 10
+			end
+		end
+		marketSwapType = "Decrease"
+		CloseMarketScreen(screen, button)
+		OpenMarketScreen()
+	elseif marketSwapType == "Decrease" then
+		for itemIndex, item in ipairs( CurrentRun.MarketItems ) do
+			if item.Priority then
+				item.CostAmount = item.CostAmount / 10
+				item.BuyAmount = item.BuyAmount / 10
+			end
+		end
+		marketSwapType = "Increase"
+		CloseMarketScreen(screen, button)
+		OpenMarketScreen()
+	end
+end
