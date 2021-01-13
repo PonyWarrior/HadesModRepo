83a8cade7b29ed4628dfdea0a1885c8a25d4b8b7
Unknown <lob.revolu@gmail.com>
2021-01-13 23:33:19 +0100

    added config, biome outlines, exclusions and fix armored enemies

--- Colorblind/colorblind.lua
+++ Colorblind/colorblind.lua
@@ -1,20 +1,89 @@
+local config =
+{
+    ExcludeTraps = false,
+    ExcludeMiscs = false,
+}
+
+local Outlines =
+{
+    -- Fallback if the biome name is somehow nil
+    Default =
+    {
+        R = 50,
+        G = 50,
+        B = 255,
+        Opacity = 0.8,
+        Thickness = 1,
+        Threshold = 0.6,
+    },
+    Tartarus =
+    {
+        R = 50,
+        G = 50,
+        B = 255,
+        Opacity = 0.8,
+        Thickness = 1,
+        Threshold = 0.6,
+    },
+    Asphodel =
+    {
+        R = 50,
+        G = 50,
+        B = 255,
+        Opacity = 0.8,
+        Thickness = 1,
+        Threshold = 0.6,
+    },
+    Elysium =
+    {
+        R = 50,
+        G = 50,
+        B = 255,
+        Opacity = 0.8,
+        Thickness = 1,
+        Threshold = 0.6,
+    },
+    Styx =
+    {
+        R = 50,
+        G = 50,
+        B = 255,
+        Opacity = 0.8,
+        Thickness = 1,
+        Threshold = 0.6,
+    },
+    -- Only for Greece, dad fight is in Styx
+    Surface =
+    {
+        R = 50,
+        G = 50,
+        B = 255,
+        Opacity = 0.8,
+        Thickness = 1,
+        Threshold = 0.6,
+    },
+}
+
+function IsOutlineLegal(enemy)
+    if config.ExcludeTraps and enemy.InheritFrom ~= nil and Contains(enemy.InheritFrom, "BaseTrap") then
+        return false
+    end
+
+    if config.ExcludeMiscs and enemy.InheritFrom ~= nil and Contains(enemy.InheritFrom, "BaseBreakable") then
+        return false
+    end
+
+    return true
+end
 
 function CreateLevelDisplay( newEnemy, currentRun )
-    -- mod start
-    local outline =
-    {
-        R = 105,
-        G = 0,
-        B = 255,
-        Opacity = 1.0,
-        Thickness = 2,
-        Threshold = 0.6,
-    }
-
-    newEnemy.Outline = outline
-    newEnemy.Outline.Id = newEnemy.ObjectId
-    AddOutline( newEnemy.Outline )
-    -- mod end
+    -- Mod start
+    if IsOutlineLegal(newEnemy) then
+        newEnemy.Outline = Outlines[CurrentRun.CurrentRoom.RoomSetName] or Outlines.Default
+        newEnemy.Outline.Id = newEnemy.ObjectId
+        AddOutline( newEnemy.Outline )
+    end
+    -- Mod end
 
 	if newEnemy.HideLevelDisplay or newEnemy.HasLevelDisplay then
 		return
@@ -41,3 +110,35 @@ function CreateLevelDisplay( newEnemy, currentRun )
 		end
 	end
 end
+
+function DoEnemyHealthBufferDeplete( enemy )
+	if enemy.OnHealthBufferDepleteFunctionName ~= nil then
+		_G[enemy.OnHealthBufferDepleteFunctionName]( enemy )
+    end
+    -- Mod start
+    -- RemoveOutline({ Id = enemy.ObjectId })
+    -- Mod end
+	if enemy.TetherIds ~= nil then
+		for k, tetherId in ipairs( enemy.TetherIds ) do
+			RemoveOutline({ Id = tetherId })
+		end
+	end
+	if not enemy.WasImmuneToStunWithoutArmor then
+		SetUnitProperty({ Property = "ImmuneToStun", Value = false, DestinationId = enemy.ObjectId })
+	end
+	ApplyEffectFromWeapon({ Id = CurrentRun.Hero.ObjectId, DestinationId = enemy.ObjectId, WeaponName = "ArmorBreakAttack", EffectName = "ArmorBreakStun" })
+end
+
+OnAnyLoad{function ()
+    while 1 == 1 do
+	    local ammoIds = GetIdsByType({ Name = "AmmoPack" })
+        if ammoIds ~= nil then
+            for _, id in pairs (ammoIds) do
+                local outline = Outlines[CurrentRun.CurrentRoom.RoomSetName] or Outlines.Default
+                outline.Id = id
+                AddOutline( outline )
+            end
+        end
+        wait(1)
+    end
+end}
