de9d624489e29858a3418871f682ba81c2092d31
Unknown <lob.revolu@gmail.com>
2021-09-16 11:21:05 +0200

    fix door text

--- ChallengeMod/challengemain.lua
+++ ChallengeMod/challengemain.lua
@@ -29,6 +29,7 @@ local HellModePactOptions =
 
 OnAnyLoad{"RoomPreRun", function(triggerArgs)
     local selector = DeepCopyTable( ObstacleData.SecretDoor )
+    selector.UseText = "{I} Open Challenges"
     selector.ObjectId = SpawnObstacle({ Name = "ShrinePointDoor", Group = "FX_Terrain", DestinationId = CurrentRun.Hero.ObjectId, AttachedTable = selector, OffsetX = -1250, OffsetY = -1150 })
     SetupObstacle( selector )
     AddToGroup({Id = selector.ObjectId, Name = "ChallengeSelector"})
